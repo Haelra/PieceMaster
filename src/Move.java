26ced77a25f9b1c0edac86a5619cddedc9da6aad
Haelra <Haelra@Haelra-Lappi>
2012-04-23 16:36:20 +0200

    move add comments etc

--- src/Move.java
+++ src/Move.java
@@ -6,6 +6,14 @@ public class Move {
      * Method "toString()"
    * "Board.move(Move)" */
 	public Move(String move) {
+		// Add method "Board.move(String)" that takes a string of the form "a5-b4"
+		move.charAt(0);
+		
+	}
+	public Move(Square from, Square to) {
+		//that moves the piece at the first argument to the location at the
+	    //second.  For now, no checks are done.  Make sure to
+	    //update side and move number.
 		
 	}
 	
