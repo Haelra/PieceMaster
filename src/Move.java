b50d8815d09e5c42f9f4e9a53302b32043cd19a2
Haelra <Haelra@Haelra-Lappi>
2012-04-23 16:21:08 +0200

    added methods

--- src/Move.java
+++ src/Move.java
@@ -1,4 +1,16 @@
 
 public class Move {
-
+	 /*New class Move
+     * public Square fields "from", "to"
+     * Constructor "Move(String)"
+     * Method "toString()"
+   * "Board.move(Move)" */
+	public Move(String move) {
+		
+	}
+	
+	public String toString() {
+		return "";
+	}
 }
+
